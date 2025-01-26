b7152c6d8a41a4654595b1eabf9990ef4ec9cc19
SiteSprinkle <pcsimpletonguide@gmail.com>
2025-01-26 20:31:12 +0530

    fff

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('Hello!');
+function disableExiting() {
+    window.onbeforeunload = function() {
+        return "Are you sure you want to leave?";
+    };
+
+    window.addEventListener('keydown', function (e) {
+        if (e.key === "F4" || (e.ctrlKey && (e.key === 'w' || e.key === 't' || e.key === 'W' || e.key === 'T'))) {
+            e.preventDefault();
+        }
+    });
+
+    document.body.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+    }
+}
+
+function enableInteraction() {
+    document.body.style.pointerEvents = 'auto';
+    window.onbeforeunload = null;
+}
+
+disableExiting();
+
+let timerDuration = 50000;
+setTimeout(enableInteraction, timerDuration);
